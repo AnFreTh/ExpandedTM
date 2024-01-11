c6475e3f79beb12b40668818c3766e085ca80675
AFThielmann <antonthielmann@t-online.de>
2024-01-11 17:43:49 +0100

    adapt dataset path + upload embedding

--- ExpandedTM/data_utils/dataset.py
+++ ExpandedTM/data_utils/dataset.py
@@ -7,6 +7,11 @@ import re
 from octis.preprocessing.preprocessing import Preprocessing
 
 
+import os
+import pickle
+import pandas as pd
+
+
 class TMDataset(OCTISDataset):
     def __init__(self):
         super().__init__()
@@ -23,14 +28,6 @@ class TMDataset(OCTISDataset):
             # Otherwise, load it from the given path
             super().fetch_dataset(name)
 
-    @staticmethod
-    def get_package_dataset_path(relative_path):
-        # Define the base path to your package's dataset directory
-        base_path = "../preprocessed_datasets/"
-
-        # Combine the base path with the relative path
-        return os.path.join(base_path, relative_path)
-
     def get_dataframe(self):
         self.dataframe = pd.DataFrame(
             {
@@ -40,16 +37,24 @@ class TMDataset(OCTISDataset):
         )
         self.dataframe["text"] = [" ".join(words) for words in self.dataframe["tokens"]]
 
-    def get_embeddings(self, embedding_model_name):
-        # Get the directory of the current script
-        script_dir = os.path.dirname(__file__)
+    @staticmethod
+    def get_package_dataset_path(relative_path):
+        # Define the base path to your package's dataset directory
+        base_path = os.path.join(os.path.dirname(__file__), "preprocessed_datasets")
 
-        # Construct an absolute path to the embeddings file
+        # Combine the base path with the relative path
+        return os.path.join(base_path, relative_path)
+
+    def get_embeddings(self, embedding_model_name):
+        # Construct the dataset folder path
+        dataset_folder = self.get_package_dataset_path(self.name)
+
+        # Ensure the dataset folder exists or create it if it doesn't
+        os.makedirs(dataset_folder, exist_ok=True)
+
+        # Construct the embeddings file path
         embeddings_file = os.path.join(
-            script_dir,
-            "..",
-            "pre_embedded_datasets",
-            self.name,
+            dataset_folder,
             f"{self.name}_embeddings_{embedding_model_name}.pkl",
         )
 
