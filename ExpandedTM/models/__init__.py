0371cc7f9acb5be2569854f6b3cf11b637aab727
AFThielmann <antonthielmann@t-online.de>
2024-01-13 17:51:13 +0100

    format and cleanup imports TNTM

--- ExpandedTM/models/__init__.py
+++ ExpandedTM/models/__init__.py
@@ -3,3 +3,5 @@ from .DCTE import DCTE
 from .KmeansTM import KmeansTM
 from .som import SOMTM
 from .cbc import CBC
+from .TNTM.TNTM_bow import TNTM_bow
+from .TNTM.TNTM_SentenceTransformer import TNTM_SentenceTransformer
